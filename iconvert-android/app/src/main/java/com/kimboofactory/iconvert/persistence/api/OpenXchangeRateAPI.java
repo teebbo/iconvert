0869630826903cbc9cb3bf257e0924c65a87948c
gnemoka <gnemo.ka@gmail.com>
2019-02-18 08:34:34 +0100

    adding peach-toolbox library

--- iconvert-android/app/src/main/java/com/kimboofactory/iconvert/persistence/api/OpenXchangeRateAPI.java
+++ iconvert-android/app/src/main/java/com/kimboofactory/iconvert/persistence/api/OpenXchangeRateAPI.java
@@ -1,9 +1,13 @@
 package com.kimboofactory.iconvert.persistence.api;
 
+import com.aleengo.peach.toolbox.commons.net.OkHttpSingleton;
+import com.aleengo.peach.toolbox.commons.net.PeachRequest;
 import com.kimboofactory.iconvert.net.KFYRequest;
 import com.kimboofactory.iconvert.util.SingletonUtil;
 
 import okhttp3.Callback;
+import okhttp3.Dispatcher;
+import okhttp3.OkHttpClient;
 
 /**
  * Created by CK_ALEENGO on 13/02/2019.
@@ -16,19 +20,47 @@ public class OpenXchangeRateAPI {
     public static final String CURRENCIES_END_POINT = "currencies.json";
 
     private static OpenXchangeRateAPI instance;
+    private static OkHttpClient client;
+
+    private OpenXchangeRateAPI() {
+        client = configureClient();
+    }
+
+    private static class LazyHolder {
+        private static final OpenXchangeRateAPI INSTANCE = new OpenXchangeRateAPI();
+    }
 
     public static OpenXchangeRateAPI getInstance() {
-        instance = SingletonUtil.getInstance(OpenXchangeRateAPI.class, instance);
-        return instance;
+        return LazyHolder.INSTANCE;
     }
 
 
     public void getCurrencies(Callback callback) {
-        final KFYRequest request = KFYRequest.builder()
+        /*final KFYRequest request = KFYRequest.builder()
                 .baseUrl(BASE_URL)
                 .endpoint(CURRENCIES_END_POINT)
+                .build();*/
+
+        final PeachRequest request = new PeachRequest.Builder()
+                .client(client)
+                .baseUrl(BASE_URL)
                 .build();
 
-        request.execute(callback);
+        //request.execute(callback);
+        request.get(CURRENCIES_END_POINT, callback);
+        request.get(RATE_END_POINT, callback);
+
+
+    }
+
+    private OkHttpClient configureClient() {
+        final Dispatcher dispatcher = new Dispatcher();
+        dispatcher.setMaxRequests(5);
+
+        return OkHttpSingleton.getInstance()
+                .getClient()
+                .newBuilder()
+                .dispatcher(dispatcher)
+                .build();
     }
 }
